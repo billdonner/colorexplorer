631e8b12437de62bc78be706fa7172577ef3b90e
bill donner <billdonner@gmail.com>
2024-10-09 11:00:54 -0500

    Initial Commit

---
+++ colorexplorer/colorexplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  colorexplorerApp.swift
+//  colorexplorer
+//
+//  Created by bill donner on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct colorexplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ colorexplorerTests/colorexplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  colorexplorerTests.swift
+//  colorexplorerTests
+//
+//  Created by bill donner on 10/9/24.
+//
+
+import Testing
+@testable import colorexplorer
+
+struct colorexplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
